6b4de18c506851ae71a895a56a1056d73b2af5bb
Erik Larsson <who+github@cnackers.org>
2020-08-04 05:56:47 +0200

    Add generic get_name function and add some signature scheme mappings

--- collections/ansible_collections/whooo/tpm2/plugins/module_utils/convert.py
+++ collections/ansible_collections/whooo/tpm2/plugins/module_utils/convert.py
@@ -4,12 +4,14 @@
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.hashes import (
+    Hash,
     SHA256,
 )
 from cryptography.hazmat.primitives.asymmetric.ec import (
     EllipticCurvePublicKey,
     EllipticCurvePublicNumbers,
     SECP256R1,
+    ECDSA,
 )
 from cryptography.hazmat.primitives.asymmetric.rsa import (
     RSAPublicNumbers,
@@ -21,7 +23,15 @@ from tpm2_pytss.binding import (
     TPM2_ALG_RSA,
     TPM2_ALG_ECC,
     TPM2_ECC_NIST_P256,
+    TPM2_ALG_RSAPSS,
+    TPM2_ALG_RSASSA,
+    TPM2_ALG_ECDSA,
 )
+from cryptography.hazmat.primitives.asymmetric.padding import (
+    PSS,
+    PKCS1v15
+)
+from ansible_collections.whooo.tpm2.plugins.module_utils.marshal import marshal
 
 def buffer_to_bytes(src, attr='buffer'):
     ba = bytearray()
@@ -34,7 +44,10 @@ def buffer_to_bytes(src, attr='buffer'):
 mappings = (
     (TPM2_ALG_SHA256, SHA256, 'SHA256'),
     (TPM2_ALG_RSA, RSAPublicKey, 'RSA'),
+    (TPM2_ALG_RSAPSS, PSS, 'RSAPSS'),
+    (TPM2_ALG_RSASSA, PKCS1v15, 'RSASSA'),
     (TPM2_ALG_ECC, EllipticCurvePublicKey, 'ECC'),
+    (TPM2_ALG_ECDSA, ECDSA, 'ECDSA'),
 )
 
 def tpm2_to_crypto_alg(algid):
@@ -87,3 +100,13 @@ def public_to_crypto(public):
     elif keytype == TPM2_ALG_ECC:
         return public_ecc_to_crypto(public.publicArea)
     raise Exception("Unsupported key type: {}".format(keytype))
+
+def get_name(area):
+    halg = tpm2_to_crypto_alg(area.nameAlg)
+    algp = area.nameAlg.to_bytes(2, 'big')
+    pbuf = marshal(area)
+    h = Hash(halg(), default_backend())
+    h.update(pbuf)
+    dig = h.finalize()
+    name = algp + dig
+    return name
